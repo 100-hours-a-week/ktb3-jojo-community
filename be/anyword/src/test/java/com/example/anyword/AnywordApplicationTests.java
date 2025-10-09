0613970f5cd2e73884ce2db877b34d00e2989ae8
jieun0824 <suspen064@gmail.com>
2025-10-09 21:28:15 +0900

    chore: initial setting for anyword community-be

---
+++ be/anyword/src/test/java/com/example/anyword/AnywordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.anyword;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnywordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
