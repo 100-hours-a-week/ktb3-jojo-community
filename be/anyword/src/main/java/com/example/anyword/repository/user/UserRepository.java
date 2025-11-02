a72ea787e90951b1adcacbc804e088dd9f872627
jieun0824 <suspen064@gmail.com>
2025-11-02 16:55:44 +0900

    feat: UserRepositoryJpql 추가, 로그인 db 연동

--- be/anyword/src/main/java/com/example/anyword/repository/user/UserRepository.java
+++ be/anyword/src/main/java/com/example/anyword/repository/user/UserRepository.java
@@ -7,9 +7,9 @@ public interface UserRepository {
   Optional<UserEntity> findByEmail(String email);
   Optional<UserEntity> findById(Long id);
   UserEntity nonOptionalFindById(Long id);
+  Optional<UserEntity> findByNickname(String nickname);
   boolean isEmailExist(String email);
   boolean isNicknameExist(String nickname);
   UserEntity save(UserEntity user);
-  Optional<UserEntity> findByNickname(String nickname);
   boolean deleteById(Long id);
 }
