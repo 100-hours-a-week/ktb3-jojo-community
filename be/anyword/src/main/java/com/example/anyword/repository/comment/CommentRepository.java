e16155932cee72d53a374c4619de81948e6e74e9
jieun0824 <suspen064@gmail.com>
2025-11-09 15:16:51 +0900

    fix: commentRepository 에러 수정

--- be/anyword/src/main/java/com/example/anyword/repository/comment/CommentRepository.java
+++ be/anyword/src/main/java/com/example/anyword/repository/comment/CommentRepository.java
@@ -9,7 +9,12 @@ import org.springframework.data.repository.query.Param;
 public interface CommentRepository extends JpaRepository<CommentEntity, Long> {
   @Query("select count(c) from CommentEntity c where c.article.id in (:article_id)")
   long countByArticleId(@Param("article_id") Long articleId);
-  @Query("select count(c) from CommentEntity c where c.article.id in (:article_id) group by c.article.id")
-  List<Object[]> bulkCountByArticleId(@Param("article_id") List<Long> articleId);
+  @Query("""
+  select c.article.id, count(c)
+  from CommentEntity c
+  where c.article.id in :articleIds
+  group by c.article.id
+""")
+  List<Object[]> bulkCountByArticleId(@Param("articleIds") List<Long> articleIds);
   List<CommentEntity> findAllByArticleIdOrderByCreatedAtDesc(Long articleId);
 }
