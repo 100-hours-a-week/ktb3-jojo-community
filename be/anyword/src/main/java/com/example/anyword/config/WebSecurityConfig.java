5c8fb0a667551ba0dbfe587b8a788aad147903d4
jieun0824 <suspen064@gmail.com>
2025-11-23 16:32:06 +0900

    fix: cors Error security 방식으로 수정

---
+++ be/anyword/src/main/java/com/example/anyword/config/WebSecurityConfig.java
@@ -0,0 +1,46 @@
+package com.example.anyword.config;
+
+import java.util.Arrays;
+import java.util.List;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+
+@Configuration
+@EnableWebSecurity //Spring Security 활성화
+@EnableGlobalMethodSecurity(prePostEnabled = true) //인가 @PreAuthorize 어노테이션 사용 위해
+public class WebSecurityConfig {
+
+  @Bean
+  CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+    configuration.setAllowedOrigins(List.of("http://127.0.0.1:5500"));
+    configuration.setAllowedMethods(Arrays.asList("GET","POST", "PUT", "PATCH", "DELETE"));
+
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+
+    return source;
+  }
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+
+    httpSecurity
+//        .cors((cors) -> cors.disable()); //CORS 통합 끄기
+        .csrf(csrf -> csrf.disable());
+
+
+
+    return httpSecurity.build();
+  }
+
+
+}
