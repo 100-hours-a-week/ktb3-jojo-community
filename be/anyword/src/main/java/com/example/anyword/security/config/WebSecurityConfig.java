0c7a791d9ac539c104e55002acd554c710e92545
jieun0824 <suspen064@gmail.com>
2025-11-26 16:42:27 +0900

    feat: JwtAuthFilter 추가 및 filterChain 에 등록

--- be/anyword/src/main/java/com/example/anyword/security/config/WebSecurityConfig.java
+++ be/anyword/src/main/java/com/example/anyword/security/config/WebSecurityConfig.java
@@ -1,13 +1,17 @@
 package com.example.anyword.security.config;
 
+import com.example.anyword.security.JwtAuthFilter;
 import java.util.Arrays;
 import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -16,7 +20,10 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @Configuration
 @EnableWebSecurity(debug=true) //Spring Security 활성화
 @EnableGlobalMethodSecurity(prePostEnabled = true) //인가 @PreAuthorize 어노테이션 사용 위해
+@RequiredArgsConstructor
 public class WebSecurityConfig {
+
+  private final JwtAuthFilter jwtAuthFilter;
 
 
   @Bean
@@ -38,14 +45,13 @@ public class WebSecurityConfig {
   public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
     httpSecurity
         .cors((cors) -> {})
-        .csrf(csrf -> csrf.disable());//TODO: 임시
-//        .authorizeHttpRequests(auth -> auth
-//            .requestMatchers("/api/login").permitAll()
-//            .anyRequest().authenticated()
-//        );
-
-
-
+        .csrf(csrf -> csrf.disable())//TODO: 임시
+        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) //jwt 위해 session 만들지 않음
+        .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class) //filter 등록. filter 위치를 지정.
+        .authorizeHttpRequests(auth -> auth
+            .requestMatchers("/api/user/login", "/api/user/signup").permitAll() //토큰 없이 접근 가능
+            .anyRequest().authenticated()
+        );
     return httpSecurity.build();
   }
 
