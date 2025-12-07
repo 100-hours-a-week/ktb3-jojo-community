7773b9feff5cd70e1cfe8a90c917f7096fffc50c
jieun0824 <suspen064@gmail.com>
2025-12-07 16:08:08 +0900

    chore: cors 설정에 localhost 추가

--- src/main/java/com/example/anyword/security/config/WebSecurityConfig.java
+++ src/main/java/com/example/anyword/security/config/WebSecurityConfig.java
@@ -29,7 +29,7 @@ public class WebSecurityConfig {
   @Bean
   CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration configuration = new CorsConfiguration();
-    configuration.setAllowedOrigins(List.of("http://127.0.0.1:5500"));
+    configuration.setAllowedOrigins(List.of("http://127.0.0.1:5500", "http://localhost:5500"));
     configuration.setAllowedMethods(Arrays.asList("GET","POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
     configuration.setAllowedHeaders(List.of("*"));
     configuration.setAllowCredentials(true);
